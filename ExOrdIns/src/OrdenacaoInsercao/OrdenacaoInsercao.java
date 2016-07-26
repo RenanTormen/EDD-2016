dfb5253f2ebfe225478903193e85d17b7db6662b
aluno <aluno@labv-09.ac-cas.unipar.br>
2016-07-26 20:10:04 -0300

    Deu ruim

--- ExOrdIns/src/OrdenacaoInsercao/OrdenacaoInsercao.java
+++ ExOrdIns/src/OrdenacaoInsercao/OrdenacaoInsercao.java
@@ -10,21 +10,31 @@ public class OrdenacaoInsercao {
 
     public static void main(String[] args) {
 
-       // int vet2[] = new int[4];
+        // int vet2[] = new int[4];
         int chave = 0;
+        System.out.println("tamanho do vetor: " + vet.length);
 
-        for (int i = 0; i >= vet.length - 1; i++) {
-            System.out.println(vet[i]);
+        for (int i = 0; i <= vet.length - 1; i++) {
 
             chave = vet[i];
-            for (int j = 0; i >= vet.length - 1; i++) {
-                if (chave >= 0 && chave > vet[j + 1]) {
+
+            for (int j = 0; j <= vet.length - 2; j++) {
+
+                if (chave >= 0 && vet[j + 1] > chave) {
                     chave = vet[j + 1];
+
                 } else {
                     vet[j] = vet[j + 1];
                     vet[j + 1] = chave;
+
                 }
             }
+        }
+
+        int t = 0;
+        while (t != vet.length) {
+            System.out.println(vet[t]);
+            t++;
         }
     }
 
