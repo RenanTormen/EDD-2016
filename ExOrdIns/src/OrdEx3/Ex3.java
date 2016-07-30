649a22f340e4930f58d44fdcf271c31478650e46
aluno <aluno@labi-12.ac-cas.unipar.br>
2016-07-29 22:32:44 -0300

    Ex3 concluido

--- ExOrdIns/src/OrdEx3/Ex3.java
+++ ExOrdIns/src/OrdEx3/Ex3.java
@@ -1,10 +1,23 @@
-
 package OrdEx3;
 
-/**
- *
- * @author aluno
- */
+import javax.swing.JOptionPane;
+
 public class Ex3 {
-    
+
+    public static void main(String[] args) {
+        Vetor v = new Vetor();
+        String c = "";
+        c = JOptionPane.showInputDialog("Selecione a opção desejada: \n 1 - Ordenação do > p/ < \n 2 - Ordenação do < p/ >");
+        switch (c) {
+            case "1":
+                v.ordenaMaior(4);
+                v.mostraVetor();
+                main(args);
+            case "2" :
+                v.ordenaMenor(4);
+                v.mostraVetor();
+                main(args);
+        }
+
+    }
 }
