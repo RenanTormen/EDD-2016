19b70bb96965ee9d90eabeab0765b39a843d75bc
aluno <aluno@labi-12.ac-cas.unipar.br>
2016-07-29 21:49:30 -0300

    Deu certo neste commit. Ex 1 e 2

--- ExOrdIns/src/OrdenacaoInsercao/OrdenacaoInsercao.java
+++ ExOrdIns/src/OrdenacaoInsercao/OrdenacaoInsercao.java
@@ -8,7 +8,7 @@ public class OrdenacaoInsercao {
 
     }
 
-    public static void main(String[] args) {
+    public static void ordenaMenor() {
 
         // int vet2[] = new int[4];
         int chave = 0;
@@ -18,11 +18,13 @@ public class OrdenacaoInsercao {
         for (int i = 1; i < vet.length; i++) {
 
             chave = vet[i];
-            System.out.println("Chave: "+chave+" Vetor ["+vet[i]+"]");
+      
 
             for ( j = i-1; j >= 0 && vet[j] > chave; j--) {
+      
                 vet[j+1] = vet[j];
             }
+
             vet[j+1] = chave;
 
         }
@@ -34,4 +36,34 @@ public class OrdenacaoInsercao {
         }
     }
 
+    public static void ordenaMaior() {
+
+        // int vet2[] = new int[4];
+        int chave = 0;
+        int j = 0;
+        System.out.println("tamanho do vetor: " + vet.length);
+
+        for (int i = 1; i < vet.length; i++) {
+
+            chave = vet[i];
+
+            for ( j = i-1; j >= 0 && vet[j] < chave; j--) {
+                vet[j+1] = vet[j];
+            }
+            vet[j+1] = chave;
+       
+
+        }
+
+        int t = 0;
+        while (t != vet.length) {
+            System.out.println(vet[t]);
+            t++;
+        }
+    }
+    
+    public static void main(String[] args) {
+        ordenaMenor();
+        ordenaMaior();
+    }
 }
